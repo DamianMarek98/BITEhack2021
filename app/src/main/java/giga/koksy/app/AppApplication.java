37319a05a2a6753b286bc2b5a9c92c747c201786
Deny <zmdddamian@gmail.com>
2021-01-17 12:50:22 +0100

    heroku test

--- app/src/main/java/giga/koksy/app/AppApplication.java
+++ app/src/main/java/giga/koksy/app/AppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppApplication
 {
+	//test
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
